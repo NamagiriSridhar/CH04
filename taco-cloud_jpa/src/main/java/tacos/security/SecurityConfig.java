2455dab3ce0ff746a20356b85e04aa013c5fff3a
NamagiriSridhar <49659868+NamagiriSridhar@users.noreply.github.com>
2019-07-19 21:26:35 -0500

    Update SecurityConfig.java

--- taco-cloud_jpa/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud_jpa/src/main/java/tacos/security/SecurityConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
@@ -36,31 +38,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	  }
 	 @Override
 	  protected void configure(HttpSecurity http) throws Exception {
-	    http
-	      .authorizeRequests()
-	        .antMatchers("/design", "/orders")
-	  
-	          .access("hasRole('ROLE_USER')") //does not work as "USER"
-	        //.hasAuthority("ROLE_USER") //works as USER or ROLE_USER Or  must match with User class. If above does not work try this
-	        .antMatchers("/", "/**").access("permitAll")
-	        
-	      .and()
-	        .formLogin()
-	          .loginPage("/login")
-	        //  .loginProcessingUrl("/login") redundant
-	          .defaultSuccessUrl("/design")
-	      .and()
-	        .logout()
-	      .and()
-	        .csrf()
-	          .ignoringAntMatchers("/console/**")
-	      .and()  
-	        .headers()
-	          .frameOptions()
-	            .sameOrigin()
-	      ;
-	  }
 
+		 http.authorizeRequests().antMatchers("/console/**").permitAll().antMatchers("/login").permitAll()
+			.antMatchers("/register").permitAll().antMatchers("/styles.css")
+			.permitAll().antMatchers().hasAuthority("ROLE_USER").anyRequest().authenticated().and().csrf().disable()
+			.formLogin().loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/design")
+			.usernameParameter("username").passwordParameter("password").and().logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").and()
+			.exceptionHandling();
+
+	http.headers().frameOptions().disable(); 
+		 //below stopped working
+//	    http
+//	      .authorizeRequests()
+//	        .antMatchers("/design", "/orders")
+//	  
+//	          .access("hasRole('ROLE_USER')") //does not work as "USER"
+//	        //.hasAuthority("ROLE_USER") //works as USER or ROLE_USER Or  must match with User class. If above does not work try this
+//	        .antMatchers("/", "/**").access("permitAll")
+//	        
+//	      .and()
+//	        .formLogin()
+//	          .loginPage("/login")
+//	        //  .loginProcessingUrl("/login") redundant
+//	          .defaultSuccessUrl("/design")
+//	      .and()
+//	        .logout()
+//	      .and()
+//	        .csrf()
+//	          .ignoringAntMatchers("/console/**")
+//	      .and()  
+//	        .headers()
+//	          .frameOptions()
+//	            .sameOrigin()
+//	      ;
+	  }
+	 @Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+		}
 
 
 
